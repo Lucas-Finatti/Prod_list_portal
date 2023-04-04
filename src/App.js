f75846f391437dd2f3013fdf4e260bb45467ee94
unknown <lucas.finatti@hotmail.com>
2023-04-04 15:06:18 -0300

    eror fixed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect } from "react";
-import api from "./api/api";
 
 function App() {
-  useEffect(() => {
-    api.get('/product').then(res=>{
-      console.log(res)
-    })
-  }, [])
-
   return (
     <Routes> 
       <Route path='/' element={<Product/>}/>
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navigation from '../navigation/navigation'
+import axios from "axios";
+import { Col, Row } from "react-bootstrap";
 
 const Product = () => {
+
     return (
         <>
             <Navigation></Navigation>
             <h1>Product</h1>
+            
         </>
     )
 }
